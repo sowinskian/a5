8888df30c2c8a0f21cc6578a6dad3651d9361a01
Andrew Sowinski <32100713+sowinskian@users.noreply.github.com>
2021-03-12 10:20:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Button, Paper, List, Slider } from '@material-ui/core';
+import { Button, Paper, List, Slider } from '@material-ui/core';
 import './App.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -11,7 +11,9 @@ const useStyles = makeStyles((theme) => ({
     },
     '& .MuiButton-root': {
       margin: theme.spacing(1),
-      width: '14%',
+      marginLeft: '8.8%',
+      marginRight: '8.8%',
+      width: '15.5%',
     },
     '& .MuiSlider-root': {
       margin: theme.spacing(1),
@@ -28,17 +30,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const request = require('request');
-//const uuidv4 = require('uuid/v4');
 const { v4: uuidv4 } = require('uuid');
 
 export default function App() {
   const scrollRef = useRef(null);
   const classes = useStyles();
-  const [value, setValue] = React.useState('');
+  const [valueLeft, setValueLeft] = React.useState('');
+  const [valueRight, setValueRight] = React.useState('');
   const [messages, setMessages] = React.useState({
     "sent": []
   });
 
+  console.log(process.env.REACT_APP_TRANS_API_KEY)
   function translate(lang, sender, text) {
     let options = {
       method: 'POST',
@@ -66,7 +69,14 @@ export default function App() {
       tempJson.sent.push({ "sender": sender, "text": body[0].translations[0].text })
       setMessages(tempJson)
       scrollRef.current.scrollIntoView({ behaviour: "smooth" });
-      setValue("")
+      if(sender === "left")
+      {
+        setValueLeft("")
+      }
+      else
+      {
+        setValueRight("")
+      }
     });
   };
 
@@ -94,11 +104,11 @@ export default function App() {
         <form className={classes.root} noValidate autoComplete="off">
           <h1 style={{ color: '#beb9b0' }}>GatorCom App</h1>
           <div>
-            <Button style={{ backgroundColor: '#b17316', color: 'white', width: "22%"}} onClick={() => window.open("https://campusmap.ufl.edu/#/", "_blank")} variant="contained">Campus Map / Mapa del Campus</Button>
-            <Button style={{ backgroundColor: '#b17316', color: 'white', width: "22%" }} onClick={() => window.open("https://ufl.qualtrics.com/jfe/form/SV_07cOrzYQpSmS0bI", "_self")} variant="contained">End Interaction / Interacción Final</Button>
+            <Button style={{ backgroundColor: '#b17316', color: 'white', width: "22%", marginLeft: '2%', marginRight: '2  %'}} onClick={() => window.open("https://campusmap.ufl.edu/#/", "_blank")} variant="contained">Campus Map / Mapa del Campus</Button>
+            <Button style={{ backgroundColor: '#b17316', color: 'white', width: "22%", marginLeft: '2%', marginRight: '2%' }} onClick={() => window.open("https://ufl.qualtrics.com/jfe/form/SV_07cOrzYQpSmS0bI", "_self")} variant="contained">End Interaction / Interacción Final</Button>
           </div>
           <div style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Paper style={{ width: '60%', height: '40ch', overflow: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#3d3d3d' }}>
+            <Paper style={{ width: '60%', height: '35ch', overflow: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#3d3d3d' }}>
               {
                 messages.sent.length === 0 &&
                 <div>
@@ -109,11 +119,11 @@ export default function App() {
               <List style={{ padding: '10px' }}>
                 {
                   messages.sent.map(item => item.sender === "left" ? (
-                    <div style={{ textAlign: 'left', backgroundColor: "#2a8cfd", maxWidth: '45%', borderStyle: 'solid', borderWidth: '1px', padding: '5px', borderRadius: '10px', marginTop: '4px' }}>
+                    <div style={{ textAlign: 'left', backgroundColor: "#2170cc", maxWidth: '45%', borderStyle: 'solid', borderWidth: '1px', padding: '5px', borderRadius: '10px', marginTop: '4px' }}>
                       <text style={{ color: "white" }} display="inline">{item.text}</text>
                     </div>
                   ) : (
-                    <div style={{ textAlign: 'left', backgroundColor: '#17b817', maxWidth: '45%', borderStyle: 'solid', borderWidth: '1px', padding: '5px', borderRadius: '10px', marginTop: '4px', alignSelf: 'right', marginLeft: 'auto' }}>
+                    <div style={{ textAlign: 'left', backgroundColor: '#0f8a0f', maxWidth: '45%', borderStyle: 'solid', borderWidth: '1px', padding: '5px', borderRadius: '10px', marginTop: '4px', alignSelf: 'right', marginLeft: 'auto' }}>
                       <text style={{ color: "white" }} display="inline">{item.text}</text>
                     </div>
                   ))
@@ -124,13 +134,13 @@ export default function App() {
               <br></br>
               <li ref={scrollRef} style={{ color: '#3d3d3d' }} />
             </Paper>
-            <Paper style={{ width: '60%', height: '11ch', overflow: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#3d3d3d' }}>
+            <Paper style={{ width: '60%', height: '11%', overflow: 'auto', marginLeft: 'auto', marginRight: 'auto', backgroundColor: '#3d3d3d' }}>
               <h1 style={{ color: '#beb9b0', fontSize: '16px' }}>Translation Quality / Calidad de Traducción</h1>
             <div style={{ display: "flex", flexDirection: "row", alignContent: "center", marginLeft: 'auto', marginRight: 'auto'}}>
-                <div style={{ width: '40%', paddingTop: '0px', paddingLeft:'15px', marginTop: '4px' }}>
+                <div style={{ width: '40%', paddingTop: '0px', paddingLeft:'1%', marginTop: '4px' }}>
                   <Slider
                     defaultValue={5}
-                    style={{ color: "#2a8cfd"}}
+                    style={{ color: "#2170cc"}}
                     aria-labelledby="discrete-slider"
                     valueLabelDisplay="auto"
                     step={1}
@@ -139,11 +149,11 @@ export default function App() {
                     max={10}
                   />
                 </div>
-                <div style={{ width: '40%', paddingTop: '0px', paddingLeft:'13%', marginTop: '4px' }}>
+                <div style={{ width: '40%', paddingTop: '0px', paddingLeft:'16%', marginTop: '4px', alignSelf: 'right' }}>
 
                   <Slider
                     defaultValue={5}
-                    style={{ color: "#17b817"}}
+                    style={{ color: "#0f8a0f"}}
                     aria-labelledby="discrete-slider"
                     valueLabelDisplay="auto"
                     step={1}
@@ -157,17 +167,27 @@ export default function App() {
           </div>
           <div>
             <textarea
-              placeholder="Texto Para Traducir/ Text To Translate"
+              placeholder="Texto Para Traducir"
               multiline
-              style={{ backgroundColor: '#848484', borderRadius: '5px', width: '35%', fontSize: '16px', fontFamily: 'arial' }}
+              style={{ backgroundColor: '#848484', borderRadius: '5px', width: '20%', fontSize: '16px', fontFamily: 'arial', marginRight: "6.5%" }}
               rows={4}
-              value={value}
-              onChange={event => setValue(event.target.value)}
+              value={valueLeft}
+              onChange={event => setValueLeft(event.target.value)}
+              onKeyPress={event => {if(event.which === 13){setValueLeft(""); addMessage("left", valueLeft)}}}
+            />
+            <textarea
+              placeholder="Text To Translate"
+              multiline
+              style={{ backgroundColor: '#848484', borderRadius: '5px', width: '20%', fontSize: '16px', fontFamily: 'arial', marginLeft: "6.5%" }}
+              rows={4}
+              value={valueRight}
+              onChange={event => setValueRight(event.target.value)}
+              onKeyPress={event => {if(event.which === 13){setValueRight(""); addMessage("right", valueRight)}}}
             />
           </div>
           <div>
-            <Button style={{ backgroundColor: '#2a8cfd', color: 'white' }} onClick={() => { addMessage("left", value); setValue("") }} variant="contained">Traducir al Inglés</Button>
-            <Button style={{ backgroundColor: '#17b817', color: 'white' }} onClick={() => { addMessage("right", value); setValue("") }} variant="contained">Translate to Spanish</Button>
+            <Button style={{ backgroundColor: '#2170cc', color: 'white' }} onClick={() => { addMessage("left", valueLeft); setValueLeft("") }} variant="contained">Traducir al Inglés</Button>
+            <Button style={{ backgroundColor: '#0f8a0f', color: 'white' }} onClick={() => { addMessage("right", valueRight); setValueRight("") }} variant="contained">Translate to Spanish</Button>
           </div>
         </form>
       </div>
